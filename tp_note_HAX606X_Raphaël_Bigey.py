319048232c7cdb7a8c6bd4c375220e3ea4c46837
RaphBi <2raphael.bigey@gmail.com>
2024-03-13 22:14:33 +0100

    C'EST FINI BORDEL

--- tp_note_HAX606X_Raphaël_Bigey.py
+++ tp_note_HAX606X_Raphaël_Bigey.py
@@ -34,8 +34,9 @@ print("Liste des itérés: ",q1)
 print("Dernier des itérés: ",q1[-1])
 #L'algorithme converge bien vers le point (1,-1) minimum global de la fonction
 
-
+#Affichage de la surface de f_test
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.set_title(r"Surface de $f_{test}$")
 X = np.arange(-5, 5, 0.25)
 Y = np.arange(-5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
@@ -52,7 +53,6 @@ plt.show()
 
 # %%
 # QUESTION 2 Application au cas quadratique
-
 
 def fab(a,b,x,y):
     return y**2/a + x**2/b
@@ -90,10 +90,10 @@ ax.set_yscale("log")
 ax.set_ylabel(r"$ f_{\alpha,\beta}(x_k) $")
 ax.set_xlabel("Itérations")
 
-ax.plot(np.linspace(1, len(X), len(X)), Y,label='a=1=b')
-ax.plot(np.linspace(1, len(X2), len(X2)), Y2,label='a=10=b')
-ax.plot(np.linspace(1, len(X3), len(X3)), Y3,label='a=50=b')
-ax.plot(np.linspace(1, len(X4), len(X4)), Y4,label='a=100=b')
+ax.plot(np.linspace(1, len(X), len(X)), Y,label=r'$\alpha=1=\beta$')
+ax.plot(np.linspace(1, len(X2), len(X2)), Y2,label=r'$\alpha=10=\beta$')
+ax.plot(np.linspace(1, len(X3), len(X3)), Y3,label=r'$\alpha=50=\beta$')
+ax.plot(np.linspace(1, len(X4), len(X4)), Y4,label=r'$\alpha=100=\beta$')
 ax.legend()
 
 #%%
@@ -120,7 +120,7 @@ plt.ylabel('y')
 plt.title(r"Lignes de niveau de la fonction $ f_{1,1} $")
 plt.colorbar()
 plt.grid(True)
-plt.scatter(x_li1,y_li1,color='red',label='Les points itérés',marker='.')
+plt.scatter(x_li1,y_li1,color='red',label=r'$x_k$',marker='.')
 plt.show()
 
 
@@ -133,7 +133,7 @@ plt.ylabel('y')
 plt.title(r"Lignes de niveau de la fonction $ f_{10,10} $")
 plt.colorbar()
 plt.grid(True)
-plt.scatter(x_li2,y_li2,color='red',label='Les points itérés',marker='.')
+plt.scatter(x_li2,y_li2,color='red',label=r'$x_k$',marker='.')
 plt.show()
 
 
@@ -146,7 +146,7 @@ plt.ylabel('y')
 plt.title(r"Lignes de niveau de la fonction $ f_{50,50} $")
 plt.colorbar()
 plt.grid(True)
-plt.scatter(x_li3,y_li3,color='red',label='Les points itérés',marker='.')
+plt.scatter(x_li3,y_li3,color='red',label=r'$x_k$',marker='.')
 plt.show()
 
 
@@ -159,7 +159,7 @@ plt.ylabel('y')
 plt.title(r"Lignes de niveau de la fonction $ f_{100,100} $")
 plt.colorbar()
 plt.grid(True)
-plt.scatter(x_li4,y_li4,color='red',label='Les points itérés',marker='.')
+plt.scatter(x_li4,y_li4,color='red',label=r'$x_k$',marker='.')
 plt.legend()
 plt.show()
 
@@ -182,7 +182,7 @@ plt.figure()
 plt.yscale("log")
 
 plt.ylabel("Distance à l'optimum")
-plt.xlabel("Itérations")
+plt.xlabel("Nombre d'itérations")
 
 plt.title('Distance à l\'optimum en norme l2')
 plt.plot(li1_norm,label=r'$\alpha=1=\beta$')
@@ -195,7 +195,7 @@ plt.show()
 
 
 # %%
-
+# Choix de deux couples (alpha,beta)
 # a = 3 ; b = 20
 x = np.linspace(-2,2,1000)
 y = np.linspace(-2,2,1000)
@@ -327,6 +327,7 @@ print(f"Temps d'exécution de la méthode par coordonnée: {temps4:.4f}, le dern
 tmp = round(temps3/temps4, 3)
 print(f"Donc la méthode par coordonnée est {tmp} fois plus rapide que la méthode classique pour a=100=b")
 
+
 #%% 
 #Affichage pour l'algo de descente classique
 x = np.linspace(-2,2,1000)
@@ -348,6 +349,8 @@ plt.grid(True)
 plt.scatter(x_li1,y_li1,color='red',label=r'$x_k$',marker='.')
 plt.legend()
 plt.show()
+
+
 #%%
 #Affichage pour l'algo de descente par coordonnées
 li2 = descenteCoordFixeFab(fabGrad, [1, 1], 0.1, 1000, 0.0001,1,1)
@@ -370,19 +373,19 @@ plt.show()
 # %%
 # QUESTION 4 Avec scipy
 # Partie a) Problème convexe
-
 from scipy.optimize import minimize
 
+#la méthode de desente de gradient ne fait pas partie du catalogue de méthodes disponibles 
+# dans la fonction minimize()
 
 #Minimisation de la fonction f_20_20 avec la méthode Nelder-Mead
 def fab_20_20(v):
     x, y = v
     return y**2/20 + x**2/20
 
-
-
 pt = (1,1)
 result1 = minimize(fab_20_20, pt, method='nelder-mead',tol=10e-10)
+
 
 print("Resultat de la minimisation pour Nelder:")
 print('Success : %s' % result1.success)
@@ -409,7 +412,7 @@ print('Solution: f_20_20(%s) = %.5f' % (solution2, evaluation2),"\n")
 print("Est ce que les deux solutions sont les même à 10e-9 près:",np.isclose(solution1,solution2, atol=10e-9, rtol=10e-9))
 
 '''On remarque dans un premier temps que les deux méthodes n'effectuent pas le même 
-nombre d'évaluation. En effet, avec la méthode Nelder-Mead minimize() 
+nombre d'évaluations. En effet, avec la méthode Nelder-Mead minimize() 
 effectue 139 itérations, tandis que pour la méthode CG (conjugate gradient)en effectue 204.
 
 Dans un second temps on remarque que la valeur du minimum renvoyé est le même: 0. 
@@ -419,7 +422,7 @@ que les deux algorithmes convergent vers "la même solution"
 
 result.success nous donne si la fonction renvoie bien un résultat
 result.message explique la cause de terminaison de l'algorithme
-result.nfev nous donne le nombre d'evaluation effectuées par minimize()
+result.nfev nous donne le nombre d'evaluations effectuées par minimize()
 '''
 
 
@@ -430,7 +433,7 @@ from IPython import get_ipython
 get_ipython().run_line_magic("matplotlib", "widget")
 from pylab import cm
 
-# Representation fonction de Rosenbrock et ses lignes de niveau sur [-5,5]
+# Representation fonction de Rosenbrock et ses lignes de niveau sur [-5,5]²
 def fr(v):
     x,y = v
     return (1-x)**2 + 100*(y-x**2)**2
@@ -457,18 +460,18 @@ fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.show()
 
 
-# Figure : lignes de niveau de f_r sur [-5,5]
+# Figure : lignes de niveau de f_r sur [-5,5]²
 plt.figure()
 plt.contour(X, Y, Z,20)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title(r'Lignes de niveau $f_r$ sur [-5,5]')
+plt.title(r'Lignes de niveau $f_r$ sur [-5,5]²')
 plt.colorbar(label=r'Valeurs de $f_r(x, y)$')
 plt.grid(True)
 plt.show()
 
 # %%
-# Représentation des lignes de niveau de f_r sur [0,1.5]
+# Représentation des lignes de niveau de f_r sur [0,1.5]²
 x2 = np.arange(0, 1.5, 0.05)
 y2 = np.arange(0, 1.5, 0.05)
 X2, Y2 = np.meshgrid(x2, y2)
@@ -478,15 +481,14 @@ plt.figure()
 plt.contour(X2, Y2, Z2,20)
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title(r'Lignes de niveau $f_r$ sur [0,1.5]')
+plt.title(r'Lignes de niveau $f_r$ sur [0,1.5]²')
 plt.colorbar(label=r'Valeurs de $f_r(x, y)$')
 plt.grid(True)
 plt.show()
 
 #Les lignes de niveau nous intdique que la fonction de Rosenbrock est 
-#plate aux alentours de (1,1) notamment sur l'axe x=y. La difficulté
-#de minimisation vient de là. En effet, à chaque itérations, l'algorithme
-#se rapproche peu du minimum global
+#plate aux alentours de (1,1). La difficulté de minimisation vient de là. 
+# En effet, à chaque itérations, l'algorithme se rapproche peu du minimum global.
 
 
 
@@ -523,7 +525,7 @@ plt.show()
 # On voit bien sur ce graphique que la distance au point (1,1) devient de 
 # plus en plus petite. Cela nous permet donc d'observer correctement la convergence
 
-# Affichage des lignes de niveau de la fonction de Rosenbrock
+
 x = np.linspace(-3,3,2000)
 y = np.linspace(-3,3,2000)
 X, Y = np.meshgrid(x, y)
@@ -547,17 +549,61 @@ plt.scatter(x_test3,y_test3,color='green',label=r'$x_k test3$',marker='.')
 plt.legend()
 plt.show()
 
-#
+# On voit bien sur le graphique des lignes de niveau que chaque suite de point x_k converge
+# vers le point (1,1). On observe bien trois trajectoires de convergences différentes.
 
 
 
 # %%
-# Utilisation de la méthode de Nelder-Mead 
-# pour minimiser la fonction de Rosenborck
+# Utilisation de la méthode de Nelder-Mead  pour minimiser la fonction de Rosenborck
 pt=(2,2)
-resu = minimize(fr, pt, method='nelder-mead',tol=10e-10)
-print(resu)
+resu = minimize(fr, pt, method='nelder-mead',tol=10e-2)
+
+
+#Comparaison en itérations des méthodes:
+def iter_descente(grad, x_init, gamma, maxiter, epsilon):
+    x = x_init
+    results = [x]
+    iterations=0
+    for i in range(1, maxiter + 1):
+        iterations+=1
+        g = grad(x)
+        if np.square(np.linalg.norm(g)) <= np.square(epsilon):  # Norme 2 (voir doc numpy)
+            break
+        else:
+            x = [x[0] - gamma * g[0], x[1] - gamma * g[1]]
+            results.append(x)
+    return iterations
+
+def iter_descenteCoordFixe(grad,x_init,gamma,n_iter,epsilon):
+    x = x_init
+    res=[x]
+    iterations=0
+    for i in range(1,n_iter+1):
+        
+        g = grad(x)
+        if np.linalg.norm(g,ord=2)**2<=epsilon**2:
+            break
+        else:
+            
+            temp = x.copy()
+            for j in range(0,len(g)):
+                iterations+=1
+                temp[j] = x[j]-gamma*g[j]
+                x=temp.copy()
+                res.append(x)
+    return iterations
 
 
 
-# %%
+iter_test1 = iter_descente(grad_f, (2,2), 0.001, 500000, 0.01)
+iter_test1_coord = iter_descenteCoordFixe(grad_f, [2,2], 0.001, 500000, 0.01)
+
+iter_minimize = resu.nit
+print("Nombre itérations descente:",iter_test1)
+print("Nombre itérations descenteCoordFixe:",iter_test1_coord)
+print("Nombre itérations fonction minimize():",iter_minimize)
+
+#On constate que nos deux algorithmes effectuent bien plus d'itérations que la fonction
+#minimize(). Cela signifie qu'il est bien plus efficace d'utiliser la fonction minimize().
+
