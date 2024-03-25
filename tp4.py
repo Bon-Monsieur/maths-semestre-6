1766a542684973432ee6187d65918eda33f97b02
RaphBi <2raphael.bigey@gmail.com>
2024-03-25 18:03:59 +0100

    question1

---
+++ tp4.py
@@ -0,0 +1,40 @@
+#%%
+import numpy as np
+from scipy.optimize import minimize
+
+# %% 
+# QUESTION 1)
+def ftest(v):
+    x,y=v
+    return (x - 1) ** 2 + 3*(y + 1)**2
+
+def descente(grad, x_init, gamma, maxiter, epsilon):
+    x = x_init
+    results = [x]
+    for i in range(1, maxiter + 1):
+        g = grad(x)
+        if np.square(np.linalg.norm(g)) <= np.square(epsilon):  # Norme 2 (voir doc numpy)
+            break
+        else:
+            x = [x[0] - gamma * g[0], x[1] - gamma * g[1]]
+            results.append(x)
+    return results
+
+def fgrad(x):
+    return (2*(x[0]-1),6*(x[1]+1))
+
+q1 = descente(grad=fgrad,x_init=(1,1),gamma=0.01,maxiter=500,epsilon=10e-3)
+print("Liste des itérés: ",q1)
+print("Dernier des itérés: ",q1[-1])
+# Le minimum est (1,-1)
+
+# Utilisation de minimize pour trouver le minimum sur R²+
+bnds = ((0,np.inf),(0,np.inf))
+res = minimize(ftest, (2,3), method='TNC',tol=10e-9,bounds=bnds)
+print(res.x)
+# bnds représente les bordes de recherche. On voit que la fonction converge
+# vers (1,0), ce qui est normal car le min est (1,-1), mais -1 n'est pas positif
+
+
+
+# %%
